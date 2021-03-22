7b2fcc11e079f2aeabd712bc8cf07b88271e4841
LukaDijan <58662250+LukaDijan@users.noreply.github.com>
2021-03-22 19:11:18 +0100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Nešto novo");
+            Console.WriteLine("novo");
         }
     }
 }
